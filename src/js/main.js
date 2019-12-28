8d453b04efefebd3bb6b1214dc82f9b1c8440e4f
Miranda Hardy <mhardy@seattletimes.com>
2019-12-27 16:02:09 -0800

    basic sorting

--- src/js/main.js
+++ src/js/main.js
@@ -4,24 +4,50 @@ setTimeout(() => paywall(12345678), 5000);
 require("component-responsive-frame/child");
 require("component-leaflet-map");
 
-var element = document.querySelector("leaflet-map");
-var L = element.leaflet;
-var map = element.map;
 
+var cityHTML = document.querySelector(".city");
+var selectCity = document.querySelector(".city select");
+var selectGender = document.querySelector(".gender select");
+var mapElement = document.querySelector("leaflet-map");
+var L = mapElement.leaflet;
+var map = mapElement.map;
 
 var mapData = require("./test.geo.json");
 var pointData = require("../../data/Sheet1.sheet.json");
-// "geometry": { "type": "Point", "coordinates": [ 47.5992456, -122.3276425 ] } }
+var cities = [];
+
 for(var x = 0; x<pointData.length; x++){
   pointData[x].geometry = {"type": "Point", "coordinates": [ pointData[x].lat, pointData[x].lng  ]};
+  if(cities.indexOf(pointData[x].city) < 0){
+    cities.push(pointData[x].city);
+  }
 }
-console.log(pointData);
 
+function makeSelect(label, list){
+  var elementHTML = "<option value='' selected disabled hidden>" + label + "</option>"
+  for(var x = 0; x < list.length; x++){
+    elementHTML += "<option value='"+ list[x] +"'>"+list[x]+"</option>";
+  }
+  return elementHTML;
+}
 
-var mapElement = document.querySelector("leaflet-map");
+selectCity.innerHTML = makeSelect("City", cities);
 
-var L = mapElement.leaflet;
-var map = mapElement.map;
+var filters;
+function filter(){
+  filters = {"City": "", "Gender": ""};
+  if(selectCity.value != "City"){
+    filters["City"] = (selectCity.value);
+  }
+  if(selectGender.value != "Gender"){
+    filters["Gender"] = (selectGender.value);
+  }
+  // add age and cause
+  addMarks();
+}
+
+selectCity.addEventListener("change", filter);
+selectGender.addEventListener("change", filter);
 
 map.scrollWheelZoom.disable();
   
@@ -40,34 +66,47 @@ function geojsonMarkerOptions(feature) {
   }
 };  
 
-
 var markers = [];
 var markergroup = L.featureGroup()
 
-for( var x = 0; x<pointData.length; x++){
-  var marker = L.circleMarker([pointData[x].lat, pointData[x].lng]);
-
-    markers.push(marker);
-    marker.addTo(markergroup);
-}
+function addMarks(){
+  markergroup.clearLayers();
+  console.log(filters);
+  for( var x = 0; x<pointData.length; x++){
+    var marker = null;
+    var mark = true;
+    if(!(filters.Gender != "" && pointData[x].victim_gender == filters.Gender)){
+      console.log(pointData[x].victim_gender);
+      mark = false;
+    }
+    if(!(filters.City != "" && pointData[x].city == filters.City && mark != false)){
+      mark = false;
+    }
+    if(mark){
+      marker = L.circleMarker([pointData[x].lat, pointData[x].lng]);
+      markers.push(marker);
+      marker.addTo(markergroup);
+    }
+  }
   markergroup.addTo(map);
-  
-   map.scrollWheelZoom.disable();
+}
 
 
-   var acc = document.getElementsByClassName("accordion");
-   var i;
-   
-   for (i = 0; i < acc.length; i++) {
-     acc[i].addEventListener("click", function() {
-       this.classList.toggle("active");
-       var panel = this.nextElementSibling;
-       if (panel.style.maxHeight) {
-         panel.style.maxHeight = null;
-       } else {
-         panel.style.maxHeight = panel.scrollHeight + "px";
-       }
-     });
-   }
+map.scrollWheelZoom.disable();
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
   
   
